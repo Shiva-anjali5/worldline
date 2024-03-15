4188f85ee53e99fefcd2c7536f69bcba0147cbbe
Shiva-anjali5 <128413700+Shiva-anjali5@users.noreply.github.com>
2024-03-16 04:34:42 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,21 @@
+
+import static java.lang.System;
+public class positive
+{
+    public static void main(String args[])
+    {
+
+        int n;
+        n=10;
+        if(n!=0){
+            if(n>0)
+            {
+                out.println("Positive number");
+            }
+            else
+            out.println("Negative Number");
+        }
+        out.println("The number is zero");
+
+    }
+}
